644cb95cb2c9eb76b0666778a4277273de284c9e
Manappa Kalmani <manappa.kalmani@qualitestgroup.com>
2023-10-06 13:39:04 +0530

    lasts

--- src/test/java/smaple/FirstTest.java
+++ src/test/java/smaple/FirstTest.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.Assert;
+import org.testng.Assert.*;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -14,14 +14,11 @@ public class FirstTest {
 public void testing() {
 	System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 	
-	ChromeOptions option = new ChromeOptions();
-	option.addArguments("--start-maximized");
-	option.addArguments("--remote-allow-origins=*");
-
-	WebDriver driver = new ChromeDriver(option);
+	
+	WebDriver driver = new ChromeDriver();
 
 	driver.get("https://manappa12sk.azurewebsites.net/webapp/");
 	String expect=driver.findElement(By.xpath("//h1[normalize-space()='RahulShettyAcademy.com Learning']")).getText().trim();
-Assert.assertEquals("RahulShettyAcademy.com Learning", expect);
+System.out.println(expect);
 }
 }
